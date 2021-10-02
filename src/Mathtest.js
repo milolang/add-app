90b60aa1610ad7f8d595b212d39e8ecede3346e9
milolang <milolang@gmail.com>
2021-10-02 02:07:04 -0700

    Added correct string data for POST to API, fixed API callback

--- src/Mathtest.js
+++ src/Mathtest.js
@@ -2,35 +2,35 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./Mathtest.css";
 
-// const axios = require('axios').default;
 const url = "http://api.mathjs.org/v4/";
-// var answer = document.getElementById("answer");
 
 let equation = {
   "expr": [
-        "a = 2 + 5",
-        "a = 3 + 3",
-        "a = 1 + 2"
-        ]
+    "a = 2 + 5",
+    "a = 3 + 3",
+    "a = 6 + 2",
+    "a = 3 + 6",
+    "a = 2 + 2",
+    "a = 1 + 6"
+  ]
 };
 
-// let string = JSON.stringify(equation);
-
+//Randomizer function for equation
 function pickRandomProperty(obj) {
   var prop, len = 0, equation, pos = 0;
   for (prop in obj) {
-      if (obj.hasOwnProperty(prop)) {
-          len += 1;
-      }
+    if (obj.hasOwnProperty(prop)) {
+      len += 1;
+    }
   }
   equation = Math.floor(Math.random() * len);
   for (prop in obj) {
-      if (obj.hasOwnProperty(prop)) {
-          if (pos === equation) {
-              return prop;
-          }
-          pos += 1;
+    if (obj.hasOwnProperty(prop)) {
+      if (pos === equation) {
+        return prop;
       }
+      pos += 1;
+    }
   }
 }
 
@@ -38,10 +38,19 @@ class Mathtest extends Component {
   constructor(props) {
     super(props);
 
+    //State saves mathtest which is random eqauation and value is students answer 
     this.state = {
       mathtest: "",
+      mathtestorginal: "",
       value: "",
     };
+
+    //Randomized equation cleaned up, student sees this
+    var newEq = pickRandomProperty(equation.expr);
+    var randEq = equation.expr[newEq];
+    this.state.mathtestorginal = randEq;
+    var randEqVis = randEq.slice(3);
+    this.state.mathtest = randEqVis;
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -49,6 +58,7 @@ class Mathtest extends Component {
 
   handleChange(event) {
     this.setState({ value: event.target.value });
+    // console.log("handle change");
   }
 
   handleSubmit(event) {
@@ -56,11 +66,12 @@ class Mathtest extends Component {
 
     var newEq = pickRandomProperty(equation.expr);
     var randEq = equation.expr[newEq];
+    var randEqVis = randEq.slice(3);
     this.setState((state) => {
-      state.mathtest = randEq;
-      return {count: state.mathtest};
+      state.mathtest = randEqVis;
+      state.mathtestorginal = randEq;
+      return { count: state.mathtest };
     });
-
     event.preventDefault();
   }
 
@@ -68,18 +79,15 @@ class Mathtest extends Component {
     return (
       <React.Fragment>
         <h1>Please answer the following equation</h1>
-        <div id="random">{this.state.mathtest}</div>
 
         <form onSubmit={this.handleSubmit} id="form1">
-          <label>
-            
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-              id="answer"
-            />
-          </label>
+          <label id="random">{this.state.mathtest + " = "}</label>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            id="answer"
+          />
           <button type="submit" id="postButton" value="Submit">Evaluate</button>
         </form>
       </React.Fragment>
@@ -87,41 +95,43 @@ class Mathtest extends Component {
   }
 
   componentDidMount() {
+    console.log("Did Mount Comp");
+    // let origEqstr = JSON.stringify(this.state.mathtestorginal);
+    let origEqstr = this.state.mathtestorginal;
+    origEqstr = origEqstr.replace(/\s+/g, '');
+
+    console.log(origEqstr);
 
     axios({
       method: "post",
       url: url,
       data: {
-          // prob
+        "expr": [origEqstr]
       },
-      contentType: 'application/json'
     })
       .then((res) => {
         //handle success
-        // console.log(res);
+        console.log(res.data.result);
+        console.log(res);
       })
       .catch((error) => {
         //handle error
-        // console.log(error);
+        console.log(error);
       })
-    //   .then(function () {
-    //     //always executed
-    //   });
+      .then(function () {
+        //always executed
+      });
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // Typical usage (don't forget to compare props):
-
     //prevState shows previous state, what the student entered before
-    // console.log(prevState);
-
     if (this.state.value !== prevProps.value) {
+
+      //Student clicks Evaluate button 
       let answer = this.state.value;
       let post = document.getElementById("postButton");
-
       post.onclick = function () {
-      console.log("Did Update Comp " + answer);
-      // console.log(this.state.mathtest);
+        console.log("Did Update Comp " + answer);
 
       };
 
